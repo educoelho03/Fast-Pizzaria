2db4d02358a680dd7dc5a55274072f4dbeeadc5d
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-10-10 11:50:53 -0300

    7 commit

--- fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/controller/CardapioController.java
+++ fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/controller/CardapioController.java
@@ -1,16 +1,10 @@
 package br.com.pizzaria.fastPizzas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/cardapio")
 public class CardapioController {
 
-    @GetMapping("/")
-    public ModelAndView listaCardapio(){
-        return new ModelAndView("index"); // Retorna uma pagina HTML
-    }
 }
