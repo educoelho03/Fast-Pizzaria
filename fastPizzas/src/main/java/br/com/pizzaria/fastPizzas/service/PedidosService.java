b2544e86f08e4d1265df957fcecb2fcf149451dc
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-10-11 10:38:53 -0300

    implementando PedidosService

--- fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/PedidosService.java
+++ fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/PedidosService.java
@@ -3,6 +3,8 @@ package br.com.pizzaria.fastPizzas.service;
 import br.com.pizzaria.fastPizzas.entity.Pedidos;
 import br.com.pizzaria.fastPizzas.enums.StatusPedido;
 import br.com.pizzaria.fastPizzas.repository.PedidosRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,32 +31,23 @@ public class PedidosService {
         return pedido.getStatusPedido();
     }
 
-    public void alterarStatusPedidoById(Pedidos pedidos) throws Exception {
-        Optional<Pedidos> pedidoExiste = pedidosRepository.findById(pedidos.getId());
-
-        if (pedidoExiste.isPresent()) {
-            Pedidos pedido = pedidoExiste.get();
-            pedido.setStatusPedido(pedidos.getStatusPedido());
-            pedidosRepository.saveAndFlush(pedido);
-        } else {
-            throw new Exception("Não foi possivel alterar o status do pedido.");
+    public void alterarStatusPedidoById(Pedidos pedidos){
+        try {
+            Pedidos pedidoExiste = pedidosRepository.getReferenceById(pedidos.getId());
+            pedidoExiste.setStatusPedido(pedidos.getStatusPedido());
+            pedidosRepository.saveAndFlush(pedidoExiste);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Não foi possível alterar o status do pedido. Pedido não encontrado.");
         }
+
 
         // TODO: Corrigir.
         public void alterarInformacoesDoPedidoById(Pedidos pedidos){
-            Optional<Pedidos> pedidoExiste = pedidosRepository.findById(pedidos.getId());
+                Pedidos pedidoExiste = pedidosRepository.findById(pedidos.getId()).orElseThrow(() -> new EntityNotFoundException("Pedido não encontrado."));
+                pedidoExiste.setInformacoesPedido(pedidos.getInformacoesPedido());
+                pedidosRepository.saveAndFlush(pedidoExiste);
 
-            if (pedidoExiste.isPresent()) {
-                Pedidos pedido = pedidoExiste.get();
-                pedido.setInformacoesPedido(pedidos.getInformacoesPedido());
-
-                pedidosRepository.saveAndFlush(pedidos);
-            } else {
-                throw new Exception("Nao foi possivel alterar as informações do pedido");
-            }
         }
-
-
 
     }
 }
