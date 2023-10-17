94b6f4f4def5dd98f7d96e7c4aeff3439d190c42
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-10-17 13:22:15 -0300

    updating Service

--- fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/CardapioService.java
+++ fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/CardapioService.java
@@ -5,6 +5,7 @@ import br.com.pizzaria.fastPizzas.repository.CardapioRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CardapioService {
@@ -15,9 +16,23 @@ public class CardapioService {
         return cardapioRepository.findAll();
     }
 
+    public Cardapio getCardapioById(Long id){
+        return cardapioRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Item não encontrado: " + id));
+    }
+
     public void deletarItemById(Long id){
         cardapioRepository.deleteById(id);
     }
 
+    public void addItemCardapio(Cardapio cardapio){
+        cardapioRepository.save(cardapio);
+    }
 
+    public Cardapio alterarPrecoItem(Cardapio cardapio, Long id){
+        Cardapio cardapioExiste = cardapioRepository.getReferenceById(id);
+
+        cardapioExiste.setPreco(cardapio.getPreco());
+
+        return cardapioRepository.saveAndFlush(cardapioExiste);
+    }
 }
--- fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/FuncionarioService.java
+++ fastPizzas/src/main/java/br/com/pizzaria/fastPizzas/service/FuncionarioService.java
@@ -7,13 +7,13 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FuncionarioService {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
-
 
 
     @Transactional
